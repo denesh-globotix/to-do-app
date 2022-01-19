a28210603d1897f08c5ab337c3a3c93623e43683
globotix <denesh.siva@globotix.sg>
2022-01-19 23:36:45 +0800

    Up to 12:44

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 import { Text, HStack, Switch, useColorMode, ColorMode } from 'native-base'
 
 export default function ThemeToggle() {
+    const { colorMode, toggleColorMode } = useColorMode()
     return (
         <HStack space={2} alignItems="center">
             <Text>
                 Dark
             </Text>
-            <Switch isChecked={colorMode === 'light'}>
-
+            <Switch isChecked={colorMode === 'light'} onToggle={toggleColorMode}>
+                <Text>light</Text>
             </Switch>
-
         </HStack>
     )
 }
